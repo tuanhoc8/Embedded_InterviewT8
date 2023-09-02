a10ca2a60f687745c11e3beedee2189ea9e2948b
tuanhoc8 <103017941+tuanhoc8@users.noreply.github.com>
2023-09-02 18:04:48 +0700

    Add files via upload

---
+++ BaiTap/Baitap_1_9_2023/Header/chuoiso.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// @brief 
+typedef struct node{
+    char *arr;
+    uint8_t length;
+    struct node *next;
+    struct node *previous;
+}Words;
+void insertWords(char *arr, uint8_t length);
+void deleteListWords();
+void printNguoc(char *arr);
